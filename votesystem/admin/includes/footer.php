83a90be18f68d0ed65cd1d30caed3cd2ac33780f
Shristi Sisodiya <505shristi@gmail.com>
2022-05-08 23:29:55 +0530

    code

---
+++ votesystem/admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Minor Project</b>
+    </div>
+    <strong>Tech Culture; 2022 <a href="https://svvv.edu.in">Shri Vaishnav Vidyapeeth Vishwavidhyalaya</a></strong>
+</footer>
---
+++ votesystem/includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Minor Project</b>
+      </div>
+      <strong>Tech Culture; 2022 <a href="https://svvv.edu.in">Shri Vaishnav Vidyapeeth Vishwavidhyalaya</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
